499177ccde30a671dde23abb0785dfbd3f55731f
fjnicolas <fjnicolas@ugr.es>
2026-05-19 23:43:58 +0000

    add new vars

--- analysis_village/nc1p/makedf/.ipynb_checkpoints/make_nc1pdf-checkpoint.py
+++ analysis_village/nc1p/makedf/.ipynb_checkpoints/make_nc1pdf-checkpoint.py
@@ -32,8 +32,8 @@ def make_pandora_df_withlight(f, trkScoreCut=False, trkDistCut=50., cutClearCosm
         chi2_pids = []
         for plane in range(0, 3):
             trkhitdf = make_trkhitdf(f, plane)
-            if det == "SBND": ## FIXME
-                trkhitdf = trkhitdf[InFV(df = trkhitdf, inzback = 0., det = "SBND_nohighyz")]
+            #if det == "SBND": ## FIXME
+            #    trkhitdf = trkhitdf[InFV(df = trkhitdf, inzback = 0., det = "SBND_nohighyz")]
             #dqdx_redo = chi2pid.dqdx(trkhitdf, gain=det, calibrate=det, isMC=ismc)
             dedx_redo = chi2pid.dedx(trkhitdf, gain=det, calibrate=det, plane=plane, isMC=ismc)
             dedx_bias = (dedx_redo - trkhitdf.dedx) / trkhitdf.dedx
@@ -65,6 +65,38 @@ def make_pandora_df_withlight(f, trkScoreCut=False, trkDistCut=50., cutClearCosm
     # require fiducial verex
     if requireFiducial:
         slcdf = slcdf[InFV(slcdf.slc.vertex, 50)]
+
+
+    #### Charge in spheres with centers at vtx
+    pandora_df = make_pandora_df(f)
+    hitdf = make_trkhitdf_plane2(f)
+    new_columns = pd.MultiIndex.from_tuples(
+        [('pfp', 'trk', 'hit', col, '') for col in hitdf.columns]
+    )
+    hitdf.columns = new_columns
+    pfp_vtxdist_4cm_df = pandora_df[(pandora_df.pfp.dist_to_vertex < 50.) & (pandora_df.slc.is_clear_cosmic == 0)]
+    hittrk_matched_df = multicol_merge(hitdf.reset_index(), pfp_vtxdist_4cm_df.reset_index(),
+                                       left_on=[('entry', '', '', '', '', ''), ('rec.slc..index', '', '', '', '', ''), ('rec.slc.reco.pfp..index', '', '', '', '', '')],
+                                       right_on=[('entry', '', '', '', '', ''), ('rec.slc..index', '', '', '', '', ''), ('rec.slc.reco.pfp..index', '', '', '', '', '')],
+                                       how="right")
+    hittrk_matched_df = hittrk_matched_df.set_index(["entry", "rec.slc..index", "rec.slc.reco.pfp..index", "rec.slc.reco.pfp.trk.calo.2.points..index"], verify_integrity=True)
+    hittrk_matched_df = multicol_add(hittrk_matched_df, dmagdf(hittrk_matched_df.slc.vertex, hittrk_matched_df.pfp.trk.hit).rename(("pfp", "trk", "hit", "dist_to_vertex", "", "")))
+    hittrk_matched_df_4cm = hittrk_matched_df[hittrk_matched_df.pfp.trk.hit.dist_to_vertex < 4.]
+    hittrk_matched_df_3cm = hittrk_matched_df[hittrk_matched_df.pfp.trk.hit.dist_to_vertex < 3.]
+    hittrk_matched_df_2cm = hittrk_matched_df[hittrk_matched_df.pfp.trk.hit.dist_to_vertex < 2.]
+    hittrk_matched_df_1cm = hittrk_matched_df[hittrk_matched_df.pfp.trk.hit.dist_to_vertex < 1.]
+
+    sum_integ_4cm = (hittrk_matched_df_4cm.pfp.trk.hit.integral).groupby(level=[0,1]).sum()
+    sum_integ_3cm = (hittrk_matched_df_3cm.pfp.trk.hit.integral).groupby(level=[0,1]).sum()
+    sum_integ_2cm = (hittrk_matched_df_2cm.pfp.trk.hit.integral).groupby(level=[0,1]).sum()
+    sum_integ_1cm = (hittrk_matched_df_1cm.pfp.trk.hit.integral).groupby(level=[0,1]).sum()
+
+    #print(sum_integ_4cm)
+    slcdf['sum_integ_4cm'] = sum_integ_4cm
+    slcdf['sum_integ_3cm'] = sum_integ_3cm
+    slcdf['sum_integ_2cm'] = sum_integ_2cm
+    slcdf['sum_integ_1cm'] = sum_integ_1cm
+
 
     #print(slcdf.pfp.trk.chi2pid.head(50))
     return slcdf
